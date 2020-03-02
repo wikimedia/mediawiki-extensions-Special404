49195b9c41505bb30fe79b2a9b33a1ff6ef6cdd6
zoranzoki21 <zorandori4444@gmail.com>
2020-03-02 08:06:00 +0100

    Add phpcs and make it pass
    Change-Id: Ia3c3b8764806b173043044a380245000ad7c084a

--- includes/Special404.php
+++ includes/Special404.php
@@ -1,35 +1,37 @@
 <?php
 
 class Special404 extends UnlistedSpecialPage {
-	
+
 	public function __construct() {
-		parent::__construct('Error404');
+		parent::__construct( 'Error404' );
 	}
-	
+
+	/**
+	 * @param string|null $par
+	 */
 	public function execute( $par ) {
+		// phpcs:ignore MediaWiki.NamingConventions.ValidGlobalName.allowedPrefix
 		global $wgOut, $wgRequest, $egSpecial404RedirectExistingRoots;
-		
+
 		if ( $egSpecial404RedirectExistingRoots ) {
-			$t = null;
-			$titles = array(
+			$titles = [
 				$wgRequest->getRequestURL(),
-				trim($wgRequest->getRequestURL(), '/\\'),
-				urldecode($wgRequest->getRequestURL()),
-				urldecode(trim($wgRequest->getRequestURL(), '/\\')),
-			);
+				trim( $wgRequest->getRequestURL(), '/\\' ),
+				urldecode( $wgRequest->getRequestURL() ),
+				urldecode( trim( $wgRequest->getRequestURL(), '/\\' ) ),
+			];
 			foreach ( $titles as $pageTitle ) {
-				$t = Title::newFromText($pageTitle);
-				if ( is_object($t) && $t->exists() ) {
+				$t = Title::newFromText( $pageTitle );
+				if ( $t && $t->exists() ) {
 					$wgOut->redirect( $t->getFullURL(), 301 );
 					return;
 				}
 			}
 		}
-		
+
 		$this->setHeaders();
 		$wgOut->setStatusCode( 404 );
-		$wgOut->addWikiMsg( 'special404-body', trim($wgRequest->getRequestURL(), '/\\') );
-		
+		$wgOut->addWikiMsg( 'special404-body', trim( $wgRequest->getRequestURL(), '/\\' ) );
 	}
-	
+
 }
